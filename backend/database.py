b76d188237ffed38c79ff852235b4d0ce864c18f
yk Wei <ykwei7@gmail.com>
2022-06-08 01:19:15 +0800

    Created add and view functions for database

--- backend/database.py
+++ backend/database.py
@@ -3,29 +3,24 @@ import psycopg2
 from config import config
 
 
+SCHEMA = "streakBotDB"
+USERS = "users"
+HABITS = "habits"
+
+
 def connect():
     """Connect to the PostgreSQL database server"""
     conn = None
     try:
         # read connection parameters
         params = config()
-
-        # connect to the PostgreSQL server
         print("Connecting to the PostgreSQL database...")
         conn = psycopg2.connect(**params)
-
-        # create a cursor
         cur = conn.cursor()
-
-        # execute a statement
         print("PostgreSQL database version:")
         cur.execute("SELECT version()")
-
-        # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
-
-        # close the communication with the PostgreSQL
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
@@ -35,5 +30,46 @@ def connect():
             print("Database connection closed.")
 
 
+def view_all_users():
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        query = f'SELECT * FROM "{SCHEMA}".{USERS};'
+        cur.execute(query)
+        result = cur.fetchall()
+        print("Query was executed successfully.")
+        return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print("Database connection closed.")
+
+
+def add_user(userId: str) -> str:
+    """Adds user to database
+
+    Args:
+        userId (str): telegram userID of intended user
+    """
+    # TODO: Add exception when user is already added
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        add_user_query = (
+            f'INSERT INTO "{SCHEMA}".{USERS} ("userID") VALUES (\'{userId}\');'
+        )
+        print(add_user_query)
+        cur.execute(add_user_query)
+        conn.commit()
+        print("Query was executed successfully.")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
 if __name__ == "__main__":
-    connect()
+    add_user("12345")
+    print(view_all_users())
