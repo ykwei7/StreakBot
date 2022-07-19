7b21e702406dde5e74c2c656be16e4a5b738a85a
yk Wei <ykwei7@gmail.com>
2022-07-19 22:21:29 +0800

    Change config for database url

--- utils/config.py
+++ utils/config.py
@@ -7,6 +7,6 @@ from dotenv import load_dotenv
 def config():
     
     load_dotenv("../secret.env")
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    # DATABASE_URL = os.environ.get("DATABASE_URL")
+    # DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
     return DATABASE_URL
